d4ba3cec2e449a5af03472f821270c8cded9dfb9
andre-de <neubertandre@yahoo.de>
2024-11-27 10:52:22 +0100

    save

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import *
+
+def main():
+    a = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(a)
+
+if __name__ == "__main__":
+    main()
