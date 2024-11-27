b570bed73f9ddc2c806db41017573e3d8080281d
andre-de <neubertandre@yahoo.de>
2024-11-27 17:58:53 +0100

    added LeafNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,14 +9,28 @@ class HTMLNode:
         raise NotImplemented("Not implemented yet")
     
     def props_to_html(self):
-        return_string = ""
-        for x in self.props.keys():
-            return_string += (f'{x}="{self.props[x]}" ')
-
-        return return_string
+        if self.props is None:
+            return ""
+        else:
+            return_string = ""
+            for x in self.props.keys():
+                return_string += (f'{x}="{self.props[x]}" ')
+            return return_string
     
     def __repr__(self):
         return(f"Tag: {self.tag} Value: {self.value} Children: {self.children} Props: {self.props}")
     
-#class LeafNode(HTMLNode):
-    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestTHTMLNode(unittest.TestCase):
@@ -18,6 +18,13 @@ class TestTHTMLNode(unittest.TestCase):
         print(node.props_to_html())
         print(node)       
         
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
