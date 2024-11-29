ee877eeb6b1d3ec1dd173305ddf43f5a910801a5
andre-de <neubertandre@yahoo.de>
2024-11-29 11:21:43 +0100

    split_nodes

---
+++ src/split_nodes.py
@@ -0,0 +1,16 @@
+from htmlnode import *
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    total_list = []
+    for node in old_nodes:
+        if delimiter == "" or node.text_type != TextType.TEXT:
+            total_list.append(node)
+        else:
+            segments = node.text.split(delimiter)
+            for index, segment in enumerate(segments):
+                if index %2 == 0:
+                    new_node = TextNode(segment, TextType.TEXT)
+                else:
+                    new_node = TextNode(segment, text_type)
+                total_list.append(new_node)
+    return total_list  
