9bd71f00295de436b01581868fc744eca89962e6
andre-de <neubertandre@yahoo.de>
2024-12-11 12:49:46 +0100

    save

--- src/main.py
+++ src/main.py
@@ -10,15 +10,14 @@ dir_path_public = "./public"
 
 
 def main():
-    
+    #set up directory
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
-
+    #copy files
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
-
-
+    
     generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
